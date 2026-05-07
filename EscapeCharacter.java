42e465b3a7c93306558f2e666395aef1bf517290
Olisaemeka Ofodile <cosmasofodile@gmail.com>
2026-05-07 16:09:44 +0100

    PrimitiveDataType

---
+++ EscapeCharacter.java
@@ -0,0 +1,10 @@
+public class EscapeCharacter{
+		public static void main(String[] args){
+			System.out.print("When you need to use the escape characters \n ");
+			System.out.print("Use it when you want tro manipulate your cursor\n");
+			System.out.print("Use \\n when you want a newline");
+			System.out.print("Who is the father of Java?. The fater of Java is \"James Gosling\"\n");
+			System.out.print("Use this when you want a tab or identation\r");
+			System.out.print("We are using carriage return here");
+		}
+}
