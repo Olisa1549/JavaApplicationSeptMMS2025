c7486963c0f50a6ae75423738e65e4a4cfd77bbf
Olisa1519 <cosmasofodile@gmail.com>
2026-07-06 06:00:15 -0700

    Chapter 7

--- Test4.java
+++ Test4.java
@@ -17,16 +17,30 @@ public class Test4 {
 		
 		
 		boolean isStudent = true;
-		boolean isSenior;
+		boolean isSenior = false;
 		double price = 9.99;
 		
 		if(isStudent) {
-			System.out.println("You get a student discount of 10%");
-			price *= 0.9;
+			if(isSenior){
+				System.out.println("You get a student discount of 10%");
+				System.out.println("You get a senior discount of 20%");	
+				price *= 0.7;
+			}
+			else{
+				System.out.println("You get a student discount of 10%");
+				price *= 0.9;
+			}
+			
 		}
 		else{
+			if(isSenior){
+				System.out.println("You get a student discount of 20%");
+				price *= 0.8;
+			}
+			else{
 			price *= 1;
 			
+			}
 		}
 		System.out.printf("The price of a ticket is: $ %.2f",price);
 	}
