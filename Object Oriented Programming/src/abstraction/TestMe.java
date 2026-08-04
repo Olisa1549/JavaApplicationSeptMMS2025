f981b0e441b6e40dac382c7966ddefcd01b0948e
Olisa1519 <cosmasofodile@gmail.com>
2026-08-04 05:57:05 -0700

    Abstraction

---
+++ Object Oriented Programming/src/abstraction/TestMe.java
@@ -0,0 +1,14 @@
+
+package abstraction;
+
+
+public class TestMe {
+    public static void main(String[] args){
+        Student student1 = new Student("John", "Young", "09087654321",
+                "No 1 Ali Avenue, Victoria lane, Port Harcourt", 101);
+        
+        student1.read();
+        System.out.println("============================\n");
+        student1.study();
+    }
+}
